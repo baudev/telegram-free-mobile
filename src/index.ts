b2180132d4e5c8fbf6e2a8c8974dabca05cb16d4
baudev <baudev.contact@gmail.com>
2020-03-18 22:54:21 +0100

    Fixes bug concerning module exports

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+import {Communication} from "./Communication";
+import {Configuration} from "./Configuration";
+import {Server} from "./Server";
+import {Tools} from "./Tools";
+import {FreeMobile} from "./FreeMobile";
+import {Telegram} from "./Telegram";
+import {PublicImage} from "./PublicImage";
+
+export default {
+    Communication,
+    Configuration,
+    FreeMobile,
+    PublicImage,
+    Telegram,
+    Server,
+    Tools
+}
